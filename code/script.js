d7e6246418b8b5535172b3f8a3ae9062e28c0786
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2022-12-01 11:31:33 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,7 +1,19 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
+/* function toggle() {
   this.classList.toggle("active")
 }
-
+ */
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+/* document.getElementById("section1").onclick = toggle */
+
+const Section = document.querySelectorAll('.accordion .title');
+const description = document .querySelectorAll('.description')
+
+console.log(Section);
+Section.forEach((element)=>{
+  element.addEventListener('click',()=>{
+    description.forEach((element)=>{
+      element.classList.toggle('active')
+    })
+  })
+})
