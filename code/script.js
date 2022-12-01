ee5ae7dff510175003f9aa18f51b13c520d7f3b6
viktorHek <viktorkarlsson125@gmail.com>
2022-12-01 11:13:05 +0100

    done

--- code/script.js
+++ code/script.js
@@ -1,7 +1,8 @@
-// A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
+function openDescription(id) {
+  let dropdown = document.getElementById(`description${id}`);
+  if (dropdown.style.display === "flex") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "flex";
+  }
 }
-
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
