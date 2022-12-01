7b417459d6ee27dad69b37ac4ebd21881299f49b
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2022-12-01 14:00:21 +0100

    Update script.js

--- code/script.js
+++ code/script.js
@@ -6,14 +6,51 @@
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
 /* document.getElementById("section1").onclick = toggle */
 
-const Section = document.querySelectorAll('.accordion .title');
-const description = document .querySelectorAll('.description')
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+const description = document .querySelector('.description')
+const icon1 = document.querySelector('#icon1')
+const icon2 = document.querySelector('#icon2')
 
-console.log(Section);
-Section.forEach((element)=>{
-  element.addEventListener('click',()=>{
-    description.forEach((element)=>{
-      element.classList.toggle('active')
-    })
+const titels = document.querySelectorAll('.title');
+
+titels.forEach((title) => {
+  title.addEventListener('click',(e)=>{
+    e.currentTarget.classList.toggle('active')
   })
 })
+
+// section1.addEventListener('click',(e)=>{
+//   e.currentTarget.classList.toggle('active')
+
+  // Icon1.classList.toggle('none')
+  // Icon2.classList.toggle('active')
+
+  // if ( Icon1.style.display = 'block'){
+  //   Icon1.style.display = 'none'
+  //   Icon2.style.display = 'block'
+  //  }else{
+  //   Icon1.style.display = 'block'
+  //   Icon2.style.display = 'none'
+  //  }
+
+  // if ( Icon1.style.display = 'block'){
+  //   Icon1.style.display = 'none'
+  //  }
+  //  else{
+  //   Icon1.style.display = 'block'
+  //  }
+
+  //  if ( Icon2.style.display = 'none'){
+  //   Icon2.style.display = 'block'
+  //  }
+  //  else{
+  //   Icon1.style.display = 'none'
+  //  }
+  
+// })
+
+
+
+
