36087828ccadc1cf1f3f0a2ba925cc3e6645b76b
nawrazaltai <nawraz.altai@chasacademy.se>
2022-12-04 19:56:07 +0100

    task completed

--- code/script.js
+++ code/script.js
@@ -1,7 +1,30 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// };
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.querySelector("#section1").onclick = toggle;
+// document.querySelector("#section2").onclick = toggle;
+// document.querySelector("#section3").onclick = toggle;
+
+const titles = document.querySelectorAll(".title");
+
+titles.forEach((title) => {
+  title.addEventListener("click", () => {
+    title.nextElementSibling.classList.toggle('active');
+  })
+});
+
+titles.forEach((title) => {
+  title.addEventListener('mouseover', ()=>{
+    title.style.opacity = 0.7
+    title.style.cursor = 'pointer'
+  })
+})
+
+titles.forEach((title) => {
+  title.addEventListener('mouseout', ()=>{
+    title.style.opacity = 1
+  })
+})
